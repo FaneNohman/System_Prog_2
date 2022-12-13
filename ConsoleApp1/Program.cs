69846e90f3b3266bfc2fa760eb4dce8a8aa95467
FaneNohman <kingofthepixels1@gmail.com>
2022-12-13 21:02:13 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("chrome.exe", "https://github.com/");
+            Process[] Chrome = Process.GetProcessesByName("chrome");
+
+            for(int i = 0; i<Chrome.Length;i++)
+            {
+                Console.WriteLine($"{i}: {Chrome[i].ProcessName}");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
